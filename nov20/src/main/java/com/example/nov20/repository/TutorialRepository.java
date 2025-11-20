3de00acdf6f5bb9c338b667eeddd727681ded1d1
Rakesh8086 <rakeshteja7007@gmail.com>
2025-11-20 17:38:31 +0530

    Nov 20 code added - webflux

---
+++ nov20/src/main/java/com/example/nov20/repository/TutorialRepository.java
@@ -0,0 +1,15 @@
+package com.example.nov20.repository;
+
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.nov20.model.Tutorial;
+
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface TutorialRepository extends R2dbcRepository<Tutorial, Integer>{
+  Flux<Tutorial> findByTitleContaining(String title);
+  
+  Flux<Tutorial> findByPublished(boolean isPublished);
+}
